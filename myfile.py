afbbffc3b72996932dfafc4e91bc479b7bfbdeb3
atharvapatil22 <55489070+atharvapatil22@users.noreply.github.com>
2025-01-28 21:19:39 +0000

    added python code (task5)

---
+++ myfile.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    result = a + b
+        return result
+
+print("Hello World")
+
+print("Addition: ",1+1)
+print("Addition: ",12+15)
+print("Addition: ",add(1,1))
+print("Addition: ",add(12,15))
